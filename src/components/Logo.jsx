b84a5f71468b0426e540ef96ecf5fb62f2e9ce6a
Tarun-vv <tarunv1911@gmail.com>
2024-12-30 12:13:19 -0700

    logo fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ function Logo({width}) {
   return (
     <div>
       <img
-        src="../../public/hero.png"
+        src="/hero.png"
         alt=""
         style={{ width: `${width}%`, marginBottom: "30px", marginRight: "auto" }}
       />
